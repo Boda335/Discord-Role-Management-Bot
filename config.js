651bc08d741da46e2c02e4382b0852ab86466a40
abdallah mohamed <elmassry826@gmail.com>
2024-07-22 20:57:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    clientId: '1264963728160329769',
-    thumbnail: 'https://b.top4top.io/p_3125o1hyk1.png',
-    image: 'https://d.top4top.io/p_3125dzye31.png',
+    clientId: 'CLIENT_ID',
+    thumbnail: 'THUMBNAIL_URL',
+    image: 'IMAGE_URL',
     footer: {
-        text: 'Coded By boda3350',
-        iconURL: 'https://b.top4top.io/p_3125o1hyk1.png'
+        text: 'FOOTER_TEXT',
+        iconURL: 'ICON_URL'
     },
 };
